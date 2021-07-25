ca81a2bc8fa3b7347be4c87f7f76dd3f7c3f4ace
Daniel F. Alvarez Caballero <ingdanielf8@gmail.com>
2021-07-25 11:53:59 -0500

    Creacion Template Estudios

---
+++ src/components/Estudios.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h2>Estudios</h2>
+    <ul>
+      <li>
+        <p>En curso:</p>
+        <p>Ing. Informática - 5to Semestre</p>
+        <p>UNIR (Universidad Internacional de la Rioja)</p>
+      </li>
+      <li>
+        <p>Otros estudios:</p>
+        <p>Ing. Civil - Pregrado</p>
+        <p>UPB (Universidad Pontificia Bolivariana)</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Estudios",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
